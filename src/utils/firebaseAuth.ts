d71592735ff508854d0b1927ce3be6c1c319862f
scorpion712 <39129900+scorpion712@users.noreply.github.com>
2025-04-01 10:03:43 -0300

    Delete src/utils/firebaseAuth.ts

--- src/utils/firebaseAuth.ts
+++
@@ -1,8 +0,0 @@
-import * as admin from 'firebase-admin'; 
-import { config } from '../config/config';
-
-admin.initializeApp({
-  credential: admin.credential.cert(config.firebaseServiceAccountKeyPath as admin.ServiceAccount),
-});
-
-export default admin;
