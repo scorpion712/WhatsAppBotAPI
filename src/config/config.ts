fe48dc6482f9dab5d2967ba91cf489cab3636cce
scorpion712 <39129900+scorpion712@users.noreply.github.com>
2025-04-01 10:05:35 -0300

    Delete src/config directory

--- src/config/config.ts
+++
@@ -1,7 +0,0 @@
-import * as dotenv from 'dotenv';
-dotenv.config();
-
-export const config = {
-  port: process.env.PORT || 3000,
-  firebaseServiceAccountKeyPath: process.env.FIREBASE_SERVICE_ACCOUNT_KEY_PATH,
-};
