4055c798319fa0463e6e50c50349f940358dd606
scorpion712 <39129900+scorpion712@users.noreply.github.com>
2025-04-01 01:20:54 -0300

    Add files via upload

---
+++ src/controllers/whatsAppController.ts
@@ -0,0 +1,42 @@
+import { Request, Response, NextFunction } from 'express';
+const qrcode = require('qrcode-terminal');
+
+import { generateQRCode, sendMessage, isClientReady } from '../services/whatsAppService.ts';
+import { MessageMedia } from 'whatsapp-web.js';
+
+export const validateUser = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  try {
+    const qr = await generateQRCode();
+    
+    res.status(200).json({ qrCode: qr != "" ? qr : null });  // QR code url
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const sendMessageHandler = async (req: Request, res: Response, next: NextFunction): Promise<void> => { 
+  const { number, message, pdf } = req.body; 
+
+  try {
+    // Decode Base64 PDF
+    const pdfBuffer = Buffer.from(pdf.split(',')[1], 'base64'); // Remove 'data:application/pdf;base64,' prefix if present
+
+    // Create the media for WhatsApp
+    const media = new MessageMedia('application/pdf', pdfBuffer.toString('base64'), 'invoice.pdf');
+
+    const response = await sendMessage({number: number, message: message, media: media});
+    res.status(200).json({ message: response });
+  } catch (error) {
+    next(error);
+  }
+};
+export const isWhatsAppReady = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  try {
+    const isReady = await isClientReady();
+    
+    res.status(200).json({ isReady: isReady }); 
+  } catch (error) {
+    console.log(error)
+    next(error); 
+  }
+};
